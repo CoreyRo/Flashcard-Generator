18f67c70e817d04695b7dbae59a266acc3010740
Corey Rodems <coreyrodems@gmail.com>
2017-08-30 19:08:13 -0700

    remade ui.js to use inquirer to build the cards

--- ui.js
+++ ui.js
@@ -1,13 +1,66 @@
 //USER INTERFACE
-var cardType = process.argv[2];
 var nodeArgs = process.argv;
+var makeCard = false;
 var inquirer = require("inquirer");
-var BasicCard = require("./basicCard.js");
-var ClozeCard = require("./clozeCard.js");
 
-switch(cardType){
-    case "basic": var myBasic = new BasicCard(process.argv[3], process.argv[4]);
-    break;
-    case "cloze": var myCloze = new ClozeCard(process.argv[3], process.argv[4]);
-    break;
-}
+// Load the NPM Package inquirer
+var inquirer = require("inquirer");
+// Create a "Prompt" with a series of questions.
+inquirer
+	.prompt([
+		// Here we create a basic text prompt.
+		{
+			type: "list",
+			message: "What type of flashcard?",
+			name: "card",
+			choices: ["basic", "cloze"]
+		},
+		{
+			type: "confirm",
+			message: "Are you sure:",
+			name: "confirm",
+			default: true
+		},
+		{
+			type: "input",
+			message: "Enter the front of the flashcard. (The Question)",
+			name: "question"
+		},
+		{
+			type: "confirm",
+			message: "Are you sure:",
+			name: "confirm",
+			default: true
+		},
+		{
+			type: "input",
+			message: "Enter the back of the flashcard. (The Answer)",
+			name: "answer"
+		},
+		{
+			type: "confirm",
+			message: "Are you sure:",
+			name: "confirm",
+			default: true
+		}
+	])
+	.then(function(inquirerResponse) {
+		// If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+		if (inquirerResponse.confirm) {
+            var cardType = inquirerResponse.card;
+            var q = inquirerResponse.question;
+			var a = inquirerResponse.answer;
+			var BasicCard = require("./basicCard.js");
+			var ClozeCard = require("./clozeCard.js");
+			switch (cardType) {
+				case "basic":
+					var myBasic = new BasicCard(q, a);
+					break;
+				case "cloze":
+					var myCloze = new ClozeCard(q, a);
+					break;
+			}
+		} else {
+			console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+        }
+    });
